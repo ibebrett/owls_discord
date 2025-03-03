4ecb67e71e8b67bafafdf5328af19e996ea38631
Brett Jurman <i.be.brett@gmail.com>
2025-03-03 17:53:35 -0500

    initial commit

---
+++ owls_discord/server.py
@@ -0,0 +1,40 @@
+import argparse
+import logging
+
+import discord
+
+logging.basicConfig()
+logger = logging.getLogger()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discordclient = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    pass
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    logger.debug('Got message "%s"', message.content)
+    if message.content == 'owls':
+        await message.channel.send('hello world!')
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('discord_token')
+    parser.add_argument('--debug', action="store_true")
+    args = parser.parse_args()
+
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+    client.run(args.discord_token)
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="owls_discord",
+    version="1.0",
+    description="An experiment in building a discord bot",
+    author="SCSU CS Club",
+    packages=["owls_discord"],
+    entry_points={
+        "console_scripts": [
+            "owls_discord_server=owls_discord.server:main",
+        ]
+    },
+)
